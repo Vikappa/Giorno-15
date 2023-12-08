bc784530d8743003c711c32a0f5173ceac4dbb67
Vikappa <vincenzo.e.costantini@gmail.com>
2023-12-08 02:38:24 +0100

    Vado a ninna

--- scripts/D10.js
+++ scripts/D10.js
@@ -28,59 +28,75 @@ REGOLE
 /* ESERCIZIO C
   Crea una variabile chiamata "me" e assegnaci un oggetto contenente le seguenti proprietà: name = il tuo nome, surname = il tuo cognome, age = la tua età.
 */
-const me = {
-  name: "Vincenzo",
-  surname: "Costantini",
-  age: "29"
-}
-/* ESERCIZIO D
-  Crea del codice per rimuovere programmaticamente la proprietà "age" dall'oggetto precedentemente creato.
-*/
-const removeAge = function (obg) {
-  delete obg.age
-}
-console.log(me)
-removeAge(me)
-console.log(me)
+// const me = {
+//   name: "Vincenzo",
+//   surname: "Costantini",
+//   age: "29"
+// }
+// /* ESERCIZIO D
+//   Crea del codice per rimuovere programmaticamente la proprietà "age" dall'oggetto precedentemente creato.
+// */
+// const removeAge = function (obg) {
+//   delete obg.age
+// }
+// console.log(me)
+// removeAge(me)
+// console.log(me)
 
-/* ESERCIZIO E
-  Crea del codice per aggiungere programmaticamente all'oggetto precedentemente creato un array chiamato "skills", contenente i linguaggi di programmazione che conosci.
-*/
-const linguaggiCheConosco = ['html', 'css', 'javascript', 'java']
+// /* ESERCIZIO E
+//   Crea del codice per aggiungere programmaticamente all'oggetto precedentemente creato un array chiamato "skills", contenente i linguaggi di programmazione che conosci.
+// */
+// const linguaggiCheConosco = ['html', 'css', 'javascript', 'java']
 
-const aggiungiskill = function (target, array) {
-  target.skill = array
-}
-aggiungiskill(me, linguaggiCheConosco)
-console.log(me)
+// const aggiungiskill = function (target, array) {
+//   target.skill = array
+// }
+// aggiungiskill(me, linguaggiCheConosco)
+// console.log(me)
 
-/* ESERCIZIO F
-  Crea un pezzo di codice per aggiungere un nuovo elemento all'array "skills" contenuto nell'oggetto "me".
-*/
-const aggiungiAllaListaSkills = function (target, newSkill) {
-  target.skill.push(newSkill)
-}
-aggiungiAllaListaSkills(me, "angular")
-console.log(me)
+// /* ESERCIZIO F
+//   Crea un pezzo di codice per aggiungere un nuovo elemento all'array "skills" contenuto nell'oggetto "me".
+// */
+// const aggiungiAllaListaSkills = function (target, newSkill) {
+//   target.skill.push(newSkill)
+// }
+// aggiungiAllaListaSkills(me, "angular")
+// console.log(me)
 
-/* ESERCIZIO G
-  Crea un pezzo di codice per rimuovere programmaticamente l'ultimo elemento dall'array "skills" contenuto nell'oggetto "me".
-*/
-const togliUltimaSkill = function (target) {
-  me.skill.splice(me.skill.length - 1, 1)
-}
+// /* ESERCIZIO G
+//   Crea un pezzo di codice per rimuovere programmaticamente l'ultimo elemento dall'array "skills" contenuto nell'oggetto "me".
+// */
+// const togliUltimaSkill = function (target) {
+//   me.skill.splice(me.skill.length - 1, 1)
+// }
 
-togliUltimaSkill(me)
-console.log(me)
+// togliUltimaSkill(me)
+// console.log(me)
 // Funzioni
 
 /* ESERCIZIO 1
   Crea una funzione chiamata "dice": deve generare un numero casuale tra 1 e 6.
 */
+const dice = function () {
+  let ritorno = Math.floor(Math.random() * 6) + 1
+  return ritorno
+}
 
+// console.log(dice())
+// console.log(dice())
+// console.log(dice())
+// console.log(dice())
 /* ESERCIZIO 2
   Crea una funzione chiamata "whoIsBigger" che riceve due numeri come parametri e ritorna il maggiore dei due.
 */
+
+const whoIsBigger = function (parm1, parm2) {
+  if (parm1 >= parm2) {
+    return parm1
+  } else {
+    return parm2
+  }
+}
 
 /* ESERCIZIO 3
   Crea una funzione chiamata "splitMe" che riceve una stringa come parametro e ritorna un'array contenente ogni parola della stringa.
